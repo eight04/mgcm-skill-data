eb0d18ed452887afdc6716559f0bf80bc522b431
eight <eight04@gmail.com>
2020-11-24 18:08:19 +0800

    Add: extra damage ratio to data (#7)
    * Update readme
    
    * Change: switch to new format, add extra_dmg_ratio
    
    * Fix: some symbols are missing in the title
    
    * Fix: remove invalid data
    
    * Fix: rebuild dress name
    
    * Update readme

--- build-data.mjs
+++ build-data.mjs
@@ -38,6 +38,7 @@ doc.useApiKey("AIzaSyBmF9PBdznx-Dpxa2YOWWK6gcThwPFpLDM");
 await doc.loadInfo();
 const sheet = doc.sheetsByIndex[0];
 await sheet.loadCells("Q2:Z");
+await sheet.loadCells("AJ2:AL");
 const y = 1;
 const x = 16;
 
@@ -51,8 +52,18 @@ for (let i = y; i < sheet.rowCount; i++) {
   
   if (list.get(name.value)) continue;
   
-  const skill = [];
+  // check if there are invalid data in [AJ,AL]
   let invalid = false;
+  for (let j = 0; j < 3; j++) {
+    const cell = sheet.getCell(i, 26 + 9 + j);
+    if (cell.value && cell.effectiveFormat.textFormat.underline) {
+      invalid = true;
+      break;
+    }
+  }
+  if (invalid) continue;
+  
+  const skill = [];
   for (let j = 0; j < 3; j++) {
     const hits = sheet.getCell(i, x + 1 + j * 3);
     if (!hits.value) {
@@ -60,17 +71,23 @@ for (let i = y; i < sheet.rowCount; i++) {
       continue;
     }
     const mod = sheet.getCell(i, x + 1 + j * 3 + 1);
-    if (mod.userEnteredFormat.textFormat.underline) {
-      invalid = true;
-      break;
-    }
+    // if (mod.effectiveFormat.textFormat.underline) {
+      // invalid = true;
+      // break;
+    // }
     skill.push(
       hits.value > 1 ?
         `${mod.value} *${hits.value}` :
       mod.value
     );
   }
-  if (invalid || skill.every(s => s === 0)) continue;
+  
+  if (
+    // invalid ||
+    skill.every(s => s === 0) ||
+    skill.some(s => s === null)
+  ) continue;
+  
   list.set(name.value, skill);
 }
 
